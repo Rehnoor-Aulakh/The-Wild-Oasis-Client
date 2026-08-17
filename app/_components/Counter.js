95355abe3be1facea562b455cba4bda3947d0722
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-08-17 08:24:55 +0530

    Configured Tailwind to the NEXT.js project

---
+++ app/_components/Counter.js
@@ -0,0 +1,17 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter({ users }) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button
+      onClick={() => {
+        setCount((count) => count + 1);
+      }}
+    >
+      Count: {count}
+    </button>
+  );
+}
