2477a5349cef0736ae23060e200039e3fee6fc4f
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-09-03 08:51:36 +0530

    Finished setting up the layout and routing of the next application

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,8 @@ import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 import { Josefin_Sans } from "next/font/google";
+import Head from "next/head";
+import Header from "./_components/Header";
 
 const josefin = Josefin_Sans({
   subsets: ["latin"],
@@ -23,12 +25,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased relative`}
       >
-        <Logo />
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright © 2026 The Wild Oasis. All rights reserved.</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
